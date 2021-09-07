d0f959ddfc99a93169e835f99d820f4eba466620
antolube20 <antolube20@gmail.com>
2021-09-07 19:58:44 -0300

    add userRoutes

---
+++ servereExpress/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const router = express.Router();
+const port = 3000;
+const bodyParser = require('body-parser');
+
+app.use("/",router);
+
+app.listen(port, ()=> {
+  console.log(`App listen in port :${port}`);
+})
+
+app.use("/user").require("./routes/userRoutes");
---
+++ servereExpress/routes/userRoutes.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const router = express.Router();
+const routeController = require("../common/routeController");
+const userController = require ("../controller/userController");
+
+router.get("/",(req, res,)=> {
+  routeController.handleRequest(req, res,userController.getAll);
+});
+
+module.exports = router;
+const {exceptionType} = require("./exceptions");
+
+class AppError extends Error {
+    constructor(exceptionType, description,errorDetails) {
+        super()
+        Error.captureStackTrace(this, this.constructor)
+
+        this.appCode = exceptionType.code
+        this.message = exceptionType.message
+        this.httpStatus = exceptionType.httpStatus
+        if(description) this.description = description
+        if(errorDetails) this.errorDetails = errorDetails
+    }
+}
+
+const errorHandler = (res, ex) =>{
+    if(!(ex instanceof AppError)){
+
+        if(ex.httpStatus === 404){
+            ex = new AppError(exceptionType.notFound)
+        }else{
+            //unhandled excetion
+            ex = new AppError(exceptionType.unhandledException,ex.message)
+        }
+    }
+    const status = ex.httpStatus
+    delete ex.httpStatus
+    res.status(status).json(ex)
+}
+
+module.exports = {
+    errorHandler,
+    AppError
+}const {exceptionType} = require("./exceptions");
+
+class AppError extends Error {
+    constructor(exceptionType, description,errorDetails) {
+        super()
+        Error.captureStackTrace(this, this.constructor)
+
+        this.appCode = exceptionType.code
+        this.message = exceptionType.message
+        this.httpStatus = exceptionType.httpStatus
+        if(description) this.description = description
+        if(errorDetails) this.errorDetails = errorDetails
+    }
+}
+
+const errorHandler = (res, ex) =>{
+    if(!(ex instanceof AppError)){
+
+        if(ex.httpStatus === 404){
+            ex = new AppError(exceptionType.notFound)
+        }else{
+            //unhandled excetion
+            ex = new AppError(exceptionType.unhandledException,ex.message)
+        }
+    }
+    const status = ex.httpStatus
+    delete ex.httpStatus
+    res.status(status).json(ex)
+}
+
+module.exports = {
+    errorHandler,
+    AppError
+}
