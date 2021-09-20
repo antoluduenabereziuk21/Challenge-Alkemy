facb36af22ca968a277500f9df0d982ce5a6ba13
antolube20 <antolube20@gmail.com>
2021-09-20 19:54:43 -0300

    restore changes fail merge

---
+++ servereExpress/models/balanceModel.js
@@ -0,0 +1,47 @@
+const Sequelize = require('sequelize');
+const {sequelizeConnection} = require('../config/server/sequelizeConfig')
+const user_budget = require ('./userModel');
+
+const  BudgetModel = sequelizeConnection.define(
+    'budget',
+{
+    id_budget:{
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    budget_tipe:{
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      fields:'budget_tipe',
+    },
+    amount:{
+      type: Sequelize.FLOAT,
+      allowNull:false,
+      fields:'amount'
+
+    },
+    user_budget:{
+      type: Sequelize.INTEGER,
+      reference: {
+        model: user_budget,
+        key: 'id_user'
+      }
+    },
+    createdAt:{
+      type:Sequelize.DATE,
+      fields: 'createdAt'
+    },
+    updatedAt:{
+      type:Sequelize.DATE,
+      fields: 'updatedAt'
+    }
+},
+{
+    tableName:'budget',
+    timestamps: true
+}
+)
+
+module.exports = BudgetModel
